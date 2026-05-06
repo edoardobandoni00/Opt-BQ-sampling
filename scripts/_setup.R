089f820b7ea54223a521e581050e638e8361f7de
edoardobandoni00 <edo.bandoni00@gmail.com>
2026-05-06 11:19:54 +0200

    Add files via upload

---
+++ scripts/_setup.R
@@ -0,0 +1,8 @@
+source("R/kernels_measures.R")
+source("R/sampling.R")
+source("R/hyperparameters.R")
+source("R/bq_core.R")
+source("R/postprocess.R")
+
+dir.create("results", showWarnings = FALSE, recursive = TRUE)
+dir.create("figures", showWarnings = FALSE, recursive = TRUE)
